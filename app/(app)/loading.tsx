a8a08c98e7ac427114569ecb30318972c9582f43
Brendanmebson <brendanmebson@gmail.com>
2026-07-12 18:59:42 +0100

    f

--- app/(app)/loading.tsx
+++ app/(app)/loading.tsx
@@ -2,7 +2,7 @@ import Loader from "@/components/ui/Loader";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[400px] gap-8">
+    <div className="flex flex-col items-center justify-center min-h-[70vh] gap-8 w-full">
       <Loader />
       <p className="text-white/30 text-xs font-black uppercase tracking-widest animate-pulse">
         Loading...
